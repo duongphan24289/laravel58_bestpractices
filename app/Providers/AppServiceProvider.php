d0d266a1b596194517679aac0eec0b91b0b18466
MrPhan <duong.pv@neo-lab.vn>
2019-06-13 15:25:18 +0700

    Add telescope

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -13,7 +13,9 @@ class AppServiceProvider extends ServiceProvider
      */
     public function register()
     {
-        //
+        if($this->app->environment(['local','develop'])){
+            $this->app->register(TelescopeServiceProvider::class);
+        }
     }
 
     /**
